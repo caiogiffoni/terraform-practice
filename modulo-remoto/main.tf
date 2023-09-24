1af01564d943aece67b177dce41da851a77a89ea
caioweduu <caiogomes@weduu.com>
2023-09-24 16:40:16 -0300

    added aws remote module

---
+++ modulo-remoto/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.73.0"
+    }
+
+  }
+  backend "azurerm" {
+    resource_group_name  = "remote_state"
+    storage_account_name = "caioremotestates"
+    container_name       = "remote-state"
+    key                  = "azure-vm-modulo-remoto/terraform.tfstate"
+
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
+
+module "network" {
+  source  = "Azure/network/azurerm"
+  version = "3.5.0"
+
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
