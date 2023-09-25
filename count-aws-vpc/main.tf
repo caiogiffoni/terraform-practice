727d534eb705ee77ae0df006844a511d4ac519e6
caioweduu <caiogomes@weduu.com>
2023-09-25 19:47:43 -0300

    count aws

---
+++ count-aws-vpc/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.12.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "caio-remote-state-s2"
+    key    = "aws-vpc-count/terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "caiogiffoni"
+      manageg-by = "terraform"
+    }
+  }
+}
+
+
+
